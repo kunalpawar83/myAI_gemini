66abdb744f0c6eb3a212c8f61dca9db976afadbf
kunalpawar83 <kunalpawar8319@gmail.com>
2024-11-11 01:31:45 +0530

    forntend done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [question, setQuestion] = useState("");
+  const [response, setResponse] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(question);
+    axios
+      .post("https://backend-gemini.onrender.com/api/gemini", {
+        question: question,
+      })
+      .then((response) => {
+        setResponse(response.data.response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const speakHandler = () => {
+    const tell = new SpeechSynthesisUtterance(response);
+    window.speechSynthesis.speak(tell);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <div className="profile-pic">
+          <img
+            className="pic"
+            src={require("./assets/user.jpg")}
+            alt="profile pic"
+          />
+        </div>
+        <p className="label">Question</p>
+        <textarea
+          onChange={(e) => {
+            setQuestion(e.target.value);
+          }}
+        />
+        <button onClick={submitHandler}>Send</button>
+      </div>
+      <div className="box">
+        <div className="profile-pic">
+          <img
+            className="pic"
+            src={require("./assets/gemini.jpg")}
+            alt="profile pic"
+          />
+        </div>
+        <p className="label">Response</p>
+        <textarea value={response} />
+        <button onClick={speakHandler}>Speak</button>
+      </div>
     </div>
   );
 }
